31cf80b8db6206d2d451a2b9a237c856925fe3da
Tobias Bartsch <tobias-bartsch@gmx.net>
2020-02-01 11:10:31 -0500

    deal with faulty GTFS data in SQL queries

--- sql_queries/transit_times_and_delay.sql
+++ sql_queries/transit_times_and_delay.sql
@@ -6,15 +6,15 @@ WITH origin AS (SELECT t.unique_num, ts.stop_id, MAX(ts.stop_time) as stop_time,
 				HAVING ts.stop_id = '{0}' AND t.route_id = '{2}'
 				),
 
-destination AS (SELECT t.unique_num, ts.stop_id, MAX(ts.stop_time) as stop_time, ts.trip_update_id as tuid
+destination AS (SELECT t.unique_num, ts.stop_id, MAX(ts.stop_time) as stop_time, ts.trip_update_id as tuid, ts.delayed as delayed
 				FROM public."Train" AS t 
 					LEFT JOIN public."Trains_stopped" AS ts
 					ON t.unique_num = ts.train_unique_num
-				GROUP BY t.unique_num, ts.stop_id, ts.trip_update_id
+				GROUP BY t.unique_num, ts.stop_id, ts.trip_update_id, ts.delayed
 				HAVING ts.stop_id = '{1}' AND t.route_id = '{2}'
 				)
 
-SELECT DISTINCT  d.unique_num, d.stop_time, (d.stop_time - o.stop_time) AS transit_time
+SELECT DISTINCT d.stop_time, (d.stop_time - o.stop_time) AS transit_time, d.delayed AS delayed
 FROM origin AS o 
 	INNER JOIN destination as d
 	ON o.unique_num = d.unique_num AND o.tuid = d.tuid
@@ -24,3 +24,11 @@ FROM origin AS o
 				/* we have to "group by" to get the max stop time. sometimes trains
 				seem to stop several times at the same station -- clearly a glitch
 				in the data feed. We assume that the last stop time is the real one */
+
+
+
+
+
+
+
+	
